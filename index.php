4d97a2e4702ba3381eaf260a06a8101f5ea0d342
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-05-29 22:41:27 +0200

    logg inn

--- index.php
+++ index.php
@@ -6,28 +6,54 @@
     <title>Index</title>
 </head>
 <body>
-    <h1> Arbeidet: </h1>
+    <h2>Logg inn</h2>
     <?php
-        include "database.php" ;
-
-        $db = new Database();
-        $conn = $db->get_Connection();
-
-        $sql = "SELECT * FROM `arbeidsoppforinger`";
-
-        $result = $conn->query($sql);
-        
-
-        if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-                echo "ID: " . $row["Arbeidsid"] . " - Gjort: " . $row["Gjort"]. " - Hvor: " . $row["Hvor"]. " - Startid: " . $row["Starttid"]. " - Sluttid: " . $row["Sluttid"]. " - Dato: " . $row["Dato"].  " - Overtid: " . $row["Overtid?"]. " - Kommentar: ". $row["Kommentar"]. "<br>";
-            } 
-        }
-        else {
-            echo "0 results";
-        }
-        $conn->close();
+        session_start();
     ?>
-   
+        
+    <form method="post" style=" width: 400px;">
+        <label for="username"><b>Brukernavn: </b></label>
+        <input name="username" type="text" placeholder="Enter Username" required><br><br>
+
+        <label for="password"><b>Passord:</b></label>
+        <input name="password" type="password" placeholder="Passord" required><br><br>
+            
+        <input type="submit" name="loginBtn" value="Logg inn"><br>
+    </form>
+
+    <?php
+
+        if($_SESSION["logginOk"] == "Y"){
+            header("refresh:0.1; url=record.php");
+        }
+
+        $real_password = "Detteeretbrapassord123";
+
+        if (isset($_POST['loginBtn'])) {
+
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            
+            //Denne brukes for å sette nytt passord
+            //echo password_hash($password, PASSWORD_DEFAULT);
+
+            if($username == "Thomastj"){
+                if ($password == $real_password) {
+
+                    $_SESSION["logginOk"] = "Y";
+
+                    header("refresh:0.0; url=record.php");
+                    exit;
+
+                }
+                else{
+                    echo "Something went wrong."; 
+                }
+            }
+            else{
+                echo "Something went wrong.";  
+            }
+        }     
+    ?>
 </body>
 </html>
