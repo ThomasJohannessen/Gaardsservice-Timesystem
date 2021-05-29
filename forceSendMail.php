219df328f3d91aa8a835a09693df8d592e7c6d54
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-05-29 23:36:19 +0200

    Second atempt send mail

--- forceSendMail.php
+++ forceSendMail.php
@@ -2,25 +2,80 @@
 
     $recivers_email = "Thomas.johannessen577@gmail.com";
 
-    $secondweek = idate('W', $timestamp);
+    $secondweek = date("W"); 
     $firstweek = $secondweek - 1;
     $subject = "Timeliste for Thomas J uke: $firstweek og $secondweek";
 
     session_start();
 
-    if($_SESSION["logginOk"] != "Y"){
+    if($_SESSION["logginnOk"] != "Y"){
         header("refresh:0.0; url=index.php");
     }
 
-    
+    include "database.php";
+
+    $db = new Database();
+    $conn = $db->get_Connection();
+
+    $sql = "SELECT * FROM `arbeidsoppforinger`";
+
+    $result = $conn->query($sql);
+
     // the message
-    $msg = "First line of text\nSecond line of text";
+    $msg = "Hei!\n Under følger timelisten min for de to siste ukene.\n (Tabellen er automatisk generert. Er det noen spørsmål er det bare å ta kontakt.)\n\n Mvh \n Thomas T. Johannessen \n 40318062";
+/*
+    ?>    
+    <table>
+        <tr>
+            <th>Dato:</th>
+            <th>Gjort:</th>
+            <th>Hvor:</th>
+            <th>Starttid:</th>
+            <th>Sluttid:</th>
+            <th>Varighet:</th>
+            <th>Overtid?</th>
+            <th>Kommentar:</th>
+        </tr>
+    
+        <?php 
+    
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+    
+                $start_t = new DateTime($row['Starttid']);
+                $current_t = new DateTime($row['Sluttid']);
+                $difference = $start_t ->diff($current_t );
+                $return_time = $difference ->format('%H:%I');
+    
+                echo "<tr>";
+                    echo "<td>" . $row['Dato'] . "</td>";    
+                    echo "<td>" . $row['Gjort'] . "</td>";
+                    echo "<td>" . $row['Hvor'] . "</td>";
+                    echo "<td>" . substr($row['Starttid'],0,-10) . "</td>";
+                    echo "<td>" . substr($row['Sluttid'],0,-10) . "</td>";
+                    echo "<td>" . ($return_time) . "</td>";
+                    echo "<td>" . $row['Overtid?'] . "</td>";
+                    echo "<td>" . $row['Kommentar'] . "</td>";
+                echo "</tr>";  
+            } 
+        }
+        else {
+            echo "0 results";
+        }
+        $conn->close();
+    ?>
+    <table>
+    ";
+
+
 
     // use wordwrap() if lines are longer than 70 characters
-    $msg = wordwrap($msg,70);
+    //$msg = wordwrap($msg,70);
+*/
+   
+//<?php
 
-    echo $subject;
-    //mail($recivers_email,"My subject",$msg);
+    echo (mail($recivers_email,$subject,$msg));
 
     //header("refresh:0.0; url=record.php");
 
