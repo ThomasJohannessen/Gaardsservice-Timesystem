a3f0298b814d6d9d9de052c67daaed2c88bcf1cd
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-06-23 23:03:06 +0200

    Laget egen side for å lage rapport og print side for denne

--- generateRapport.php
+++ generateRapport.php
@@ -1,30 +1,39 @@
-<?php
-    $secondweek = date("W"); 
-    $firstweek = $secondweek - 1;
-    $subject = "Timeliste for Thomas J uke: $firstweek og $secondweek";
-
-    session_start();
-
-    if($_SESSION["logginnOk"] != "Y"){
-        header("refresh:0.0; url=index.php");
+<!DOCTYPE html>
+<html lang="nb">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" media="print" href="print.css" />
+    <title>Generere Arbeidsrapport</title>
+ 
+    <style>
+    table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
     }
 
-    echo $subject;
+    td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+    }
+    </style>
+</head>
+<body>
+    <?php
+        $fraDato = date('Y-m-d', strtotime(date('Y-m-d'). ' - 13 days'));
+        $tilDato = date('Y-m-d', strtotime(date('Y-m-d')));
 
+        $secondweek = date("W"); 
+        $firstweek = $secondweek - 1;
+        $title = "<h2> Timeliste for Thomas J uke: $firstweek og $secondweek </h2>";
 
-    include "database.php";
+        echo $title;
+    ?>
 
-    $db = new Database();
-    $conn = $db->get_Connection();
-
-    $sql = "SELECT * FROM `arbeidsoppforinger`";
-
-    $result = $conn->query($sql);
-
-    // the message
-    $msg = "Hei!\n Under følger timelisten min for de to siste ukene.\n (Tabellen er automatisk generert. Er det noen spørsmål er det bare å ta kontakt.)\n\n Mvh \n Thomas T. Johannessen \n 40318062";
-/*
-    ?>    
+    <button onclick="window.print();return false;">Generer rapport!</button>
+     
     <table>
         <tr>
             <th>Dato:</th>
@@ -32,51 +41,101 @@
             <th>Hvor:</th>
             <th>Starttid:</th>
             <th>Sluttid:</th>
-            <th>Varighet:</th>
-            <th>Overtid?</th>
-            <th>Kommentar:</th>
+            <th>Timer Normallønn:</th>
+            <th>Timer 50%</th>
+            <th>Timer 100%</th>
         </tr>
+
+    <?php 
+
+    session_start();
+
+    if($_SESSION["logginnOk"] != "Y"){
+        header("refresh:0.0; url=index.php");
+    }
+
+    include "database.php";
+
+    $db = new Database();
+    $conn = $db->get_Connection();
+
+    $sql = "SELECT * FROM `arbeidsoppforinger` WHERE Dato between '$fraDato' and '$tilDato' ORDER BY `arbeidsoppforinger`.`Arbeidsid` ASC";
     
-        <?php 
+    $result = $conn->query($sql);
     
-        if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-    
-                $start_t = new DateTime($row['Starttid']);
-                $current_t = new DateTime($row['Sluttid']);
-                $difference = $start_t ->diff($current_t );
-                $return_time = $difference ->format('%H:%I');
-    
-                echo "<tr>";
-                    echo "<td>" . $row['Dato'] . "</td>";    
-                    echo "<td>" . $row['Gjort'] . "</td>";
-                    echo "<td>" . $row['Hvor'] . "</td>";
-                    echo "<td>" . substr($row['Starttid'],0,-10) . "</td>";
-                    echo "<td>" . substr($row['Sluttid'],0,-10) . "</td>";
-                    echo "<td>" . ($return_time) . "</td>";
-                    echo "<td>" . $row['Overtid?'] . "</td>";
-                    echo "<td>" . $row['Kommentar'] . "</td>";
-                echo "</tr>";  
-            } 
+
+    $totNorTimer = 0;
+    $tot50Timer = 0;
+    $tot100Timer = 0;
+
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+
+            $date = date_create($row['Dato']);
+        
+            echo "<tr>";
+                echo "<td>" . date_format($date,"j.m - D") . "</td>";    
+                echo "<td>" . $row['Gjort'] . "</td>";
+                echo "<td>" . $row['Hvor'] . "</td>";
+                echo "<td>" . substr($row['Starttid'],0,-10) . "</td>";
+                echo "<td>" . substr($row['Sluttid'],0,-10) . "</td>";
+                echo "<td>" . ($row['TimerNormal'] != "00:00:00.000000" ? substr($row['TimerNormal'],0,-10) : " ") . "</td>";
+                echo "<td>" . ($row['Timer50'] != "00:00:00.000000" ? substr($row['Timer50'],0,-10) : " ") . "</td>";
+                echo "<td>" . ($row['Timer100'] != "00:00:00.000000" ? substr($row['Timer100'],0,-10) : " ") . "</td>";
+            echo "</tr>";
+
+            
+
+            $timeNormal = $row['TimerNormal'];
+            $time50 = $row['Timer50'];
+            $time100 = $row['Timer100'];
+
+            if($timeNormal != strtotime("00:00:00.000000")){
+                $addTimeSecNor = strtotime($timeNormal)-strtotime("00:00:00.000000");
+                $totNorTimer += $addTimeSecNor;
+            }
+            if($time50 != strtotime("00:00:00.000000")){
+                $addTimeSec50 = strtotime($time50)-strtotime("00:00:00.000000");
+                $tot50Timer += $addTimeSec50;
+            }
+            if($time100 != strtotime("00:00:00.000000")){
+                $addTimeSec100 = strtotime($time100)-strtotime("00:00:00.000000");
+                $tot100Timer += $addTimeSec100;
+            }
         }
-        else {
-            echo "0 results";
-        }
-        $conn->close();
+        
+        $minsNor = $totNorTimer % 3600;
+        $secsNor = $minsNor % 60;
+        $hoursNor = ($totNorTimer - $minsNor) / 3600;
+        $minsNor = ($minsNor - $secsNor) / 60;
+
+        $mins50 = $tot50Timer % 3600;
+        $secs50 = $mins50 % 60;
+        $hours50 = ($tot50Timer - $mins50) / 3600;
+        $mins50 = ($mins50 - $secs50) / 60;
+
+        $mins100 = $tot100Timer % 3600;
+        $secs100 = $mins100 % 60;
+        $hours100 = ($tot100Timer - $mins100) / 3600;
+        $mins100 = ($mins100 - $secs100) / 60;
+
+        echo "<tr>";
+                echo "<th>" . "Sum timer:" . "</td>";    
+                echo "<th>" . " " . "</td>";
+                echo "<th>" . " " . "</td>";
+                echo "<th>" . " " . "</td>";
+                echo "<th>" . " " . "</td>";
+                echo "<th>" . "{$hoursNor}:{$minsNor}" . "</td>";
+                echo "<th>" . "{$hours50}:{$mins50}" . "</td>";
+                echo "<th>" . "{$hours100}:{$mins100}" . "</td>";
+            echo "</tr>";  
+    }
+    else {
+        echo "";
+    }
+    $conn->close();
     ?>
-    <table>
-    ";
-
-
-
-    // use wordwrap() if lines are longer than 70 characters
-    //$msg = wordwrap($msg,70);
-*/
-   
-//<?php
-
-    echo (mail($recivers_email,$subject,$msg));
-
-    //header("refresh:0.0; url=record.php");
-
-?>
+    </table>
+    </div>
+</body>
+</html>
